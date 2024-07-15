c886dc3c7920dcfe5aafc921fb2a939d916f997f
Andrii Ivaniv <unixvb@gmail.com>
2024-07-15 13:55:03 +0300

    lint fixed

--- src/utils/api.utils.ts
+++ src/utils/api.utils.ts
@@ -3,12 +3,15 @@ import {AssetsRecordResponse} from '../types/assets-record-response.type';
 import {BestRouteResponse} from '../types/best-route-response.type';
 
 export const getAssetsRecord = () =>
-    API.get<AssetsRecordResponse>('/assets-record')
-        .then(response => response.data);
+    API.get<AssetsRecordResponse>('/assets-record').then(
+        response => response.data
+    );
 
 export const getBestRoute = (params: {
     inputAssetAmount: string;
     inputAssetAddress: string;
     outputAssetAddress: string;
-}) => API.get<BestRouteResponse>('/best-route', {params})
-    .then(response => response.data);
+}) =>
+    API.get<BestRouteResponse>('/best-route', {params}).then(
+        response => response.data
+    );
