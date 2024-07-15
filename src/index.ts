d1b6ba34cd1d33f8aa0fb55da45b66e0da2ac24b
Andrii Ivaniv <unixvb@gmail.com>
2024-07-15 14:45:48 +0300

    CalculatedSwapRoute & Asset exports added

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ export {DexTypeEnum} from './enums/dex-type.enum';
 export {RouteDirectionEnum} from './enums/route-direction.enum';
 export {SwapRouteType} from './enums/swap-route-type.enum';
 
+export type {Asset} from './types/asset.type';
+export type {CalculatedSwapRoute} from './types/calculated-swap-route.type';
+
 export {getAssetsRecord, getBestRoute} from './utils/api.utils';
 export {
     getSwapMessages,
