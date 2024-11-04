a97907292d1f070e9ddbe5f9818166f974a57e86
Andrii Ivaniv <unixvb@gmail.com>
2024-11-04 14:53:55 +0200

    abort controlling added

--- src/utils/api.utils.ts
+++ src/utils/api.utils.ts
@@ -1,5 +1,3 @@
-import {GenericAbortSignal} from 'axios';
-
 import {API} from '../globals';
 import {AppStatus} from '../types/app-status.type';
 import {Asset} from '../types/asset.type';
@@ -8,19 +6,44 @@ import {AssetsRecord} from '../types/assets-record.type';
 import {BestRouteParams} from '../types/best-route-params.type';
 import {BestRouteResponse} from '../types/best-route-response.type';
 
+export const getAppStatus = () =>
+    API.get<AppStatus>('/app-status').then(response => response.data);
+
+/**
+ * @deprecated This method is deprecated and will be removed in the next major release.
+ * Please use `getAssetsList` instead for improved performance and features.
+ */
 export const getAssetsRecord = () =>
     API.get<AssetsRecord>('/assets-record').then(response => response.data);
 
-export const getAssetsList = (params: AssetsListParams) =>
-    API.post<Asset[]>('/assets-list', params).then(response => response.data);
+// Used to cancel the previous request if it exists
+const abortControllers: Record<
+    'assetsList' | 'bestRoute',
+    AbortController | null
+> = {
+    assetsList: null,
+    bestRoute: null
+};
 
-export const getBestRoute = (
-    params: BestRouteParams,
-    signal?: GenericAbortSignal
-) =>
-    API.get<BestRouteResponse>('/best-route', {params, signal}).then(
-        response => response.data
-    );
+export const getAssetsList = (params: AssetsListParams) => {
+    if (abortControllers.assetsList) {
+        abortControllers.assetsList.abort();
+    }
+    abortControllers.assetsList = new AbortController();
 
-export const getAppStatus = () =>
-    API.get<AppStatus>('/app-status').then(response => response.data);
+    return API.post<Asset[]>('/assets-list', params, {
+        signal: abortControllers.assetsList.signal
+    }).then(response => response.data);
+};
+
+export const getBestRoute = (params: BestRouteParams) => {
+    if (abortControllers.bestRoute) {
+        abortControllers.bestRoute.abort();
+    }
+    abortControllers.bestRoute = new AbortController();
+
+    return API.get<BestRouteResponse>('/best-route', {
+        params,
+        signal: abortControllers.bestRoute.signal
+    }).then(response => response.data);
+};
