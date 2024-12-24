a808152c16b78da7951192d95d8a4b4fbb1f7d32
Andrii Ivaniv <unixvb@gmail.com>
2024-12-24 15:55:31 +0200

    Tonco pools added

--- src/enums/dex-type.enum.ts
+++ src/enums/dex-type.enum.ts
@@ -4,5 +4,6 @@ export enum DexTypeEnum {
     Ston = 'Ston',
     Ston_v2 = 'Ston_v2',
     StonStable = 'StonStable',
-    CattonStable = 'CattonStable'
+    CattonStable = 'CattonStable',
+    Tonco = 'Tonco'
 }
