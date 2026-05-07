c3662b4d14311dd103d99678d1de906b341cb007
Andrii Ivaniv <unixvb@gmail.com>
2026-05-07 18:58:22 +0800

    DeDust CpmmV2 support added

--- src/enums/dex-type.enum.ts
+++ src/enums/dex-type.enum.ts
@@ -1,6 +1,7 @@
 export enum DexTypeEnum {
     DeDust = 'DeDust',
     DeDustStable = 'DeDustStable',
+    DeDustCpmmV2 = 'DeDustCpmmV2',
     Ston = 'Ston',
     Ston_v2 = 'Ston_v2',
     StonStable = 'StonStable',
--- src/enums/swap-route-type.enum.ts
+++ src/enums/swap-route-type.enum.ts
@@ -1,6 +1,11 @@
 export enum SwapRouteType {
     DeDust = 'DeDust',
+    DeDustCpmmV2 = 'DeDustCpmmV2',
     Ston = 'Ston',
     Ston_v2 = 'Ston_v2',
-    Rainbow = 'Rainbow'
+    Rainbow = 'Rainbow',
+    CattonStable = 'CattonStable',
+    Tonco = 'Tonco',
+    Torch = 'Torch',
+    Bidask = 'Bidask'
 }
