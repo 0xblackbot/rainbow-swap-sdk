4de11d997ab85b1de44517b68a50c68accab426e
Andrii Ivaniv <unixvb@gmail.com>
2026-06-02 14:39:31 +0200

    MoonCx and Coffee support added

--- src/enums/dex-type.enum.ts
+++ src/enums/dex-type.enum.ts
@@ -1,14 +1,17 @@
 export enum DexTypeEnum {
     DeDust = 'DeDust',
-    DeDustStable = 'DeDustStable',
     DeDustCpmmV2 = 'DeDustCpmmV2',
+    DeDustStable = 'DeDustStable',
     Ston = 'Ston',
     Ston_v2 = 'Ston_v2',
     StonStable = 'StonStable',
     StonWeightedStable = 'StonWeightedStable',
-    CattonStable = 'CattonStable',
     Tonco = 'Tonco',
     Torch = 'Torch',
+    MoonCxCpmm = 'MoonCxCpmm',
+    MoonCxClmm = 'MoonCxClmm',
+    Coffee = 'Coffee',
+    CoffeeCurveFiV2 = 'CoffeeCurveFiV2',
     BidaskDamm = 'BidaskDamm',
     BidaskDlmm = 'BidaskDlmm'
 }
